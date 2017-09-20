cb3f1fc8a06996130a656b173927231681685881
wansheng <wansheng_ios@sina.com>
2017-09-20 15:37:30 +0800

    tap to dismiss

--- ImageViewer/Source/GalleryConfiguration.swift
+++ ImageViewer/Source/GalleryConfiguration.swift
@@ -11,7 +11,8 @@ import UIKit
 public typealias GalleryConfiguration = [GalleryConfigurationItem]
 
 public enum GalleryConfigurationItem {
-
+    
+    /// tap to dismiss
     case tapDismiss(Bool)
     /// Allows to stop paging at the beginning and the end of item list or page infinitely in a "carousel" like mode.
     case pagingMode(GalleryPagingMode)
