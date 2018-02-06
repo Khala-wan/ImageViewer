de1f260533040194a56ea7483838e32ca931f47d
wansheng <wansheng_ios@sina.com>
2018-02-06 16:13:59 +0800

    footer

--- ImageViewer/Source/GalleryViewController.swift
+++ ImageViewer/Source/GalleryViewController.swift
@@ -395,7 +395,6 @@ open class GalleryViewController: UIPageViewController, ItemControllerDelegate {
 
             footer.autoresizingMask = [.flexibleTopMargin, .flexibleWidth]
             footer.frame.size.width = self.view.bounds.width - marginLeft - marginRight
-            footer.sizeToFit()
             footer.frame.origin = CGPoint(x: marginLeft, y: self.view.bounds.height - footer.bounds.height - marginBottom)
 
         case .pinLeft(let marginBottom, let marginLeft):
